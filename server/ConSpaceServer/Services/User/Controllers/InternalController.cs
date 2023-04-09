7f48faf6809bdeeb0bd961d78d644abb8fafecb5
marijal74 <lakic.marija98@gmail.com>
2023-04-09 22:31:55 +0200

    [#12] Add internal controller for fetching reminders

---
+++ server/ConSpaceServer/Services/User/Controllers/InternalController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections;
+using User.DTO;
+
+namespace User.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class InternalController : ControllerBase
+{
+
+    private readonly ILogger<InternalController> _logger;
+
+    public InternalController(ILogger<InternalController> logger)
+    {
+        _logger = logger;
+    }
+
+    [Route("[action]")]
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public IEnumerable<ReminderDto> ListAllReminders(ReminderType typeFilter)
+    {
+        throw new NotImplementedException();
+    }
+
+    [Route("[action]/{id}")]
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    // returns list of reminders for a specific seminar or meet up
+    public IEnumerable<ReminderDto> GetRemindersByEventId(string id)
+    {
+        throw new NotImplementedException();
+    }
+}
