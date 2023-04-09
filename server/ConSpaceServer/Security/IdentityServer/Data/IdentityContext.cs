a12965e8aa9d83beaad4acc7b66e90f5aad308ae
Milica Radojicic <radojicicmilica98@gmail.com>
2023-04-09 20:06:47 +0200

    #9-add-updated-identity-service
    Identity service now uses .NET 6 and only has User and Administrator roles

---
+++ server/ConSpaceServer/Security/IdentityServer/Data/IdentityContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using IdentityServer.Entities;
+using IdentityServer.Data.EntityTypeConfigurations;
+
+namespace IdentityServer.Data;
+
+public class IdentityContext : IdentityDbContext<User>
+{
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+    public IdentityContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.ApplyConfiguration(new RoleConfiguration());
+        builder.ApplyConfiguration(new UserConfiguration());
+
+    }
+}
