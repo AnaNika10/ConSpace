2267950b15684d93854bb12932dd1b46da47bc12
marijal74 <lakic.marija98@gmail.com>
2023-04-20 20:32:32 +0200

    [#13] Add authorization in swagger and setup JWT consumption

--- server/ConSpaceServer/Services/User/Extensions/UserExtensions.cs
+++ server/ConSpaceServer/Services/User/Extensions/UserExtensions.cs
@@ -1,8 +1,10 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using User.Data;
 using User.DTO;
 using User.Entities;
 using User.Repositories;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace User.Extensions;
 
@@ -18,5 +20,30 @@ public static class UserExtensions
         );
         services.AddEntityFrameworkNpgsql().AddDbContext<UserContext>();
 
+    }
+
+    public static void SetupJwt(this IServiceCollection services, IConfiguration configuration)
+    {
+        // JWT Security
+        var jwtSettings = configuration.GetSection("JwtSettings");
+        var secretKey = jwtSettings.GetSection("secretKey").Value;
+        services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings.GetSection("validIssuer").Value,
+                    ValidAudience = jwtSettings.GetSection("validAudience").Value,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
+                };
+            });
     }
 }
--- server/ConSpaceServer/Services/User/Program.cs
+++ server/ConSpaceServer/Services/User/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 using User.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,8 +8,36 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(option =>
+    {
+        option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "Please enter a valid token",
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            BearerFormat = "JWT",
+            Scheme = "Bearer"
+        });
+
+        option.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                new string[] { }
+            }
+        });
+    }
+);
 builder.Services.RegisterServices();
+builder.Services.SetupJwt(builder.Configuration);
 
 var app = builder.Build();
 
@@ -19,6 +48,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
