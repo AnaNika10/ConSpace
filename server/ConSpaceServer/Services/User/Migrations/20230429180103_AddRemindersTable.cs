14ec7fd26662c8eb5da909f54b6146cf62ed94e1
marijal74 <lakic.marija98@gmail.com>
2023-04-29 20:27:30 +0200

    [#13] Add reminders table

---
+++ server/ConSpaceServer/Services/User/Migrations/20230429180103_AddRemindersTable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace User.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRemindersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "reminders",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    type = table.Column<string>(type: "text", nullable: false),
+                    timestamp = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now()"),
+                    content = table.Column<string>(type: "text", nullable: false),
+                    eventId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_reminders", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "reminders");
+        }
+    }
+}
