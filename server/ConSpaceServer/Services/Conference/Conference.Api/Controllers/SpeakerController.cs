e3108377bb9d616f0bbc5a8f1a2004dc81578112
AnaNika10 <ana.nikacevic7@gmail.com>
2023-05-01 17:38:31 +0200

    [#17] Exposed endpoints for conference, exhibitors, speakers and FAQs

---
+++ server/ConSpaceServer/Services/Conference/Conference.Api/Controllers/SpeakerController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Conference.Api.Repositories;
+using Conference.Api.DTOs.Speakers;
+
+namespace Conference.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class SpeakerController : ControllerBase
+    {
+        private readonly ISpeakersRepository _repository;
+        public SpeakerController(ISpeakersRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<SpeakerDTO>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<SpeakerDTO>>> GetAllSpeakers()
+        {
+            var conferences = await _repository.GetAllSpeakers();
+            if (conferences == null)
+            {
+                return NotFound();
+            }
+            return Ok(conferences);
+        }
+        [HttpGet("{speakerId}", Name = nameof(GetById))]
+        [ProducesResponseType(typeof(SpeakerDTO), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<SpeakerDTO>> GetById(int speakerId)
+        {
+            var conference = await _repository.GetSpeaker(speakerId);
+            if (conference == null)
+            {
+                return NotFound(null);
+            }
+            return Ok(conference);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(SpeakerDTO), StatusCodes.Status201Created)]
+        public async Task<ActionResult<SpeakerDTO>> CreateSpeaker([FromBody] CreateSpeakerDTO request)
+        {
+            int Id = await _repository.CreateSpeaker(request);
+            var speaker = await _repository.GetSpeaker(Id);
+            return CreatedAtRoute("GetById", new { speaker.SpeakerId }, speaker);
+
+        }
+        [HttpPut]
+        [ProducesResponseType(typeof(SpeakerDTO), StatusCodes.Status200OK)]
+        public async Task<ActionResult<SpeakerDTO>> UpdateSpeaker([FromBody] UpdateSpeakerDTO request)
+        {
+            await _repository.UpdateSpeaker(request);
+
+            var speaker = await _repository.GetSpeaker(request.SpeakerId);
+            return CreatedAtRoute("GetById", new { speaker.SpeakerId }, speaker);
+        }
+        [HttpDelete("{speakerId}")]
+        [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
+        public async Task<ActionResult<bool>> DeleteSpeaker(int speakerId)
+        {
+            var success = await _repository.DeleteSpeaker(speakerId);
+            if (success)
+            {
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+
+        }
+    }
+}
