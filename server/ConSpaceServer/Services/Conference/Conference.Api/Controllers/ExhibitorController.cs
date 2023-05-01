ed9c9cd5608383d99383c85b5e19e6bcd744a65e
AnaNika10 <57066689+AnaNika10@users.noreply.github.com>
2023-05-01 19:49:54 +0200

    [#17] Exposed endpoints for conference, exhibitors, speakers and FAQs (#39)

---
+++ server/ConSpaceServer/Services/Conference/Conference.Api/Controllers/ExhibitorController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Conference.Api.Repositories;
+using Conference.Api.DTOs.Exhibitors;
+
+namespace Conference.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ExhibitorController : ControllerBase
+    {
+        private readonly IExhibitorsRepository _repository;
+        public ExhibitorController(IExhibitorsRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<ExhibitorDTO>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<ExhibitorDTO>>> GetAllExhibitors()
+        {
+            var exhibitors = await _repository.GetAllExhibitors();
+            if (exhibitors == null)
+            {
+                return NotFound();
+            }
+            return Ok(exhibitors);
+        }
+        [HttpGet("{exhibitorId}", Name = nameof(GetById))]
+        [ProducesResponseType(typeof(ExhibitorDTO), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<ExhibitorDTO>> GetById(int exhibitorId)
+        {
+            var exhibitor = await _repository.GetExhibitor(exhibitorId);
+            if (exhibitor == null)
+            {
+                return NotFound(null);
+            }
+            return Ok(exhibitor);
+        }
+
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ExhibitorDTO), StatusCodes.Status201Created)]
+        public async Task<ActionResult<ExhibitorDTO>> CreatePost([FromBody] CreateExhibitorDTO request)
+        {
+            int Id = await _repository.CreateExhibitor(request);
+            var exhibitor = await _repository.GetExhibitor(Id);
+            return CreatedAtRoute("GetById", new { exhibitor.ExhibitorId }, exhibitor);
+
+        }
+        [HttpPut]
+        [ProducesResponseType(typeof(ExhibitorDTO), StatusCodes.Status200OK)]
+        public async Task<ActionResult<ExhibitorDTO>> UpdatePost([FromBody] UpdateExhibitorDTO request)
+        {
+            await _repository.UpdateExhibitor(request);
+
+            var exhibitor = await _repository.GetExhibitor(request.ExhibitorId);
+            return CreatedAtRoute("GetById", new { exhibitor.ExhibitorId }, exhibitor);
+        }
+        [HttpDelete("{exhibitorId}")]
+        [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
+        public async Task<ActionResult<bool>> DeletePost(int exhibitorId)
+        {
+            var success = await _repository.DeleteExhibitor(exhibitorId);
+            if (success)
+            {
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+
+        }
+    }
+}
---
+++ server/ConSpaceServer/Services/Conference/Conference.Api/Controllers/FAQController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Conference.Api.Repositories;
+using Conference.Api.DTOs.FAQ;
+
+namespace Conference.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class FAQController : ControllerBase
+    {
+        private readonly IFAQRepository _repository;
+        public FAQController(IFAQRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<FAQDTO>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<FAQDTO>>> GetAllFAQ()
+        {
+            var conferences = await _repository.GetAllFAQs();
+            if (conferences == null)
+            {
+                return NotFound();
+            }
+            return Ok(conferences);
+        }
+        [HttpGet("{faqId}", Name = nameof(GetById))]
+        [ProducesResponseType(typeof(FAQDTO), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<FAQDTO>> GetById(int faqId)
+        {
+            var conference = await _repository.GetFAQ(faqId);
+            if (conference == null)
+            {
+                return NotFound(null);
+            }
+            return Ok(conference);
+        }
+
+
+        [HttpPost]
+        [ProducesResponseType(typeof(FAQDTO), StatusCodes.Status201Created)]
+        public async Task<ActionResult<FAQDTO>> CreateFAQ([FromBody] CreateFAQDTO request)
+        {
+            int Id = await _repository.CreateFAQ(request);
+            var question = await _repository.GetFAQ(Id);
+            return CreatedAtRoute("GetById", new { question.QuestionId }, question);
+
+        }
+        [HttpPut]
+        [ProducesResponseType(typeof(FAQDTO), StatusCodes.Status200OK)]
+        public async Task<ActionResult<FAQDTO>> UpdateFAQ([FromBody] UpdateFAQDTO request)
+        {
+            await _repository.UpdateFAQ(request);
+
+            var question = await _repository.GetFAQ(request.QuestionId);
+            return CreatedAtRoute("GetById", new { question.QuestionId }, question);
+        }
+        [HttpDelete("{questionId}")]
+        [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
+        public async Task<ActionResult<bool>> DeleteFAQ(int questionId)
+        {
+            var success = await _repository.DeleteFAQ(questionId);
+            if (success)
+            {
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+
+        }
+    }
+}
