9cec73af6c60b1e83e888a15c7fe1d2647858eaf
marijal74 <57067366+marijal74@users.noreply.github.com>
2023-05-02 12:57:28 +0200

    [#52] Fix dockerfile, docker-compose and retry spining up the service.

--- server/ConSpaceServer/Services/User/Extensions/UserExtensions.cs
+++ server/ConSpaceServer/Services/User/Extensions/UserExtensions.cs
@@ -2,8 +2,10 @@
 
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Polly;
 using User.Data;
 using User.DTO;
 using User.Entities;
@@ -28,9 +30,32 @@ public static class UserExtensions
 
     public static void MigrateDatabase(this IApplicationBuilder app)
     {
-        using (var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+        var logger = app.ApplicationServices.GetRequiredService<ILogger<UserContext>>();
+        try
         {
-            scope.ServiceProvider.GetService<UserContext>().Database.Migrate();
+            logger.LogInformation("Migrating database associated with UserContext");
+
+            var retry = Policy.Handle<SqlException>()
+                .WaitAndRetry(
+                    retryCount: 5,
+                    sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                    onRetry: (exception, retryCount, ctx) =>
+                    {
+                        logger.LogError("Retry {RetryCount} if {PolicyKey} at {OperationKey}, due to {Exception}.", retryCount, ctx.PolicyKey, ctx.OperationKey, exception);
+                    });
+            retry.Execute(() =>
+            {
+                using (var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+                {
+                    scope.ServiceProvider.GetService<UserContext>().Database.Migrate();
+                }
+            });
+
+            logger.LogInformation("Migrating database associated with context UserContext was successful");
+        } 
+        catch (SqlException e)
+        {
+            logger.LogError(e, "An error occured while migrating the database used on context UserContext");
         }
     }
 
