9d6fecba7df1bd10f61f3b8ef34e271926c1157d
milicar7 <57152075+milicar7@users.noreply.github.com>
2023-05-07 18:31:19 +0200

    [#63] Move configuration of jwt to Common/Security project (#64)

--- server/ConSpaceServer/Security/IdentityServer/Extensions/IdentityExtensions.cs
+++ server/ConSpaceServer/Security/IdentityServer/Extensions/IdentityExtensions.cs
@@ -76,35 +76,6 @@ public static class IdentityExtensions
 
         return services;
     }
-
-    public static IServiceCollection ConfigureJWT(this IServiceCollection services, IConfiguration configuration)
-    {
-        var jwtSettings = configuration.GetSection("JwtSettings");
-        var secretKey = jwtSettings.GetSection("secretKey").Value;
-
-        services.AddAuthentication(options =>
-        {
-            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-        })
-            .AddJwtBearer(options =>
-            {
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-
-                    ValidIssuer = jwtSettings.GetSection("validIssuer").Value,
-                    ValidAudience = jwtSettings.GetSection("validAudience").Value,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
-                };
-            });
-
-        return services;
-    }
-
     public static IServiceCollection ConfigureMiscellaneousServices(this IServiceCollection services)
     {
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
