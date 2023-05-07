a6c7c690c4e6833915d96e9e5a39ec2660efec1f
marijal74 <lakic.marija98@gmail.com>
2023-05-07 22:00:13 +0200

    [#42] Configure authorization through ocelot

--- server/ConSpaceServer/SPAGateway/Program.cs
+++ server/ConSpaceServer/SPAGateway/Program.cs
@@ -1,10 +1,12 @@
+using Common.Security.Extensions;
 using Ocelot.Middleware;
 using SPAGateway.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.ConfigureServices();
-// builder.Services.
+builder.Services.ConfigureJWT(builder.Configuration);
+
 var app = builder.Build();
 
 await app.UseOcelot();
