7ae6d622ada32d80657392b802920a5b30e79a64
marijal74 <lakic.marija98@gmail.com>
2023-05-20 22:23:06 +0200

    [#43] Add tabs

--- client/src/components/SeminarList.tsx
+++ client/src/components/SeminarList.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Paper, Stack } from "@mui/material";
+import { Box, Card, Container, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Paper, Stack, Tab, Tabs } from "@mui/material";
 import TaskAltIcon from '@mui/icons-material/TaskAlt';
 import AddIcon from '@mui/icons-material/Add';
 import { useState } from "react";
@@ -23,15 +23,33 @@ function formatDate(date: Date) {
     return date.getHours().toString() + ":" + date.getMinutes().toString().padStart(2, '0');
 }
 
+function SeminarTabs() {
+    const [value, setValue] = useState('1');
+  
+    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+      setValue(newValue);
+    };
+  
+    return (
+      <Container >
+        <Tabs value={value} onChange={handleChange}>
+            <Tab label="Item One" value="1" />
+            <Tab label="Item Two" value="2" />
+        </Tabs>
+      </Container>
+    );
+}
+
 export default function SeminarList() {
     return (
         <Card>
             <Grid container justifyContent={"center"}>
                 <Paper elevation={3} >
-                <List>
-                    <SeminarListItem name="seminarcic" destination="boulevard of broken dreams" date={formatDate(new Date())}/>
-                    <SeminarListItem name="seminarcic" destination="boulevard of broken dreams" date={formatDate(new Date())}/>
-                </List>
+                    <SeminarTabs/>
+                    <List>
+                        <SeminarListItem name="seminarcic" destination="boulevard of broken dreams" date={formatDate(new Date())}/>
+                        <SeminarListItem name="seminarcic" destination="boulevard of broken dreams" date={formatDate(new Date())}/>
+                    </List>
                 </Paper>
             </Grid>
         </Card>
