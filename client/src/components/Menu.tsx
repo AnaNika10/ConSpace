a85e781f5281d189c0a12a3dede3b6a8e0d63fed
marijal74 <57067366+marijal74@users.noreply.github.com>
2023-05-28 15:44:55 +0200

    Feature/#44 my seminar calendar (#71)
    * [#43] Add navbar
    
    * [#43] Add routing. Open dialog on clicking list item button and display lecture information.
    
    * [#44] Add menu bar
    
    * [#44] Route items in menu
    
    * [#44] Add Seminar schedule component

---
+++ client/src/components/Menu.tsx
@@ -0,0 +1,150 @@
+import { Box, CssBaseline, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar } from "@mui/material";
+import { styled, useTheme, Theme, CSSObject } from '@mui/material/styles';
+import { useState } from "react";
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import TodayIcon from '@mui/icons-material/Today';
+import StickyNote2Icon from '@mui/icons-material/StickyNote2';
+import MicIcon from '@mui/icons-material/Mic';
+import MenuIcon from '@mui/icons-material/Menu'
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import MuiDrawer from '@mui/material/Drawer';
+import NavBar from "./NavigationBar";
+import { Link } from "react-router-dom";
+
+const drawerWidth = 280;
+
+const openedMixin = (theme: Theme): CSSObject => ({
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
+}
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    ...(open && {
+      ...openedMixin(theme),
+      '& .MuiDrawer-paper': openedMixin(theme),
+    }),
+    ...(!open && {
+      ...closedMixin(theme),
+      '& .MuiDrawer-paper': closedMixin(theme),
+    }),
+  }),
+);
+
+export default function NavBarAndMenu() {
+  const theme = useTheme();
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{
+              marginRight: 5,
+              ...(open && { display: 'none' }),
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <NavBar/>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="permanent" open={open}>
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <MenuList/>
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <DrawerHeader />
+      </Box>
+    </Box>
+  );
+}
+
+  const MenuList = () => <>
+  <List>
+    {[{section:'Notifications', icon: <NotificationsIcon/>, link: '/notifications'}, 
+      {section:'My Seminar Schedule', icon: <TodayIcon/>, link: '/calendar-schedule'}, 
+      {section:'Notes', icon:<StickyNote2Icon/>, link: '/notes'}, 
+      {section:'Speakers', icon: <MicIcon/>, link: 'speakers'}].map(({section, icon, link}) => (
+      <ListItem key={section}>
+          <ListItemIcon>
+              {icon}
+          </ListItemIcon>
+          <ListItemButton component = {Link} to={link}>
+            <ListItemText primary={section} />
+          </ListItemButton>
+      </ListItem>
+    ))}
+  </List> 
+</>;
---
+++ client/src/components/SeminarList.tsx
@@ -0,0 +1,105 @@
+import { AppBar, Button, Dialog, DialogActions, DialogContent, DialogTitle, Grid, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Paper, Stack, Tab, Tabs, Toolbar, Typography } from "@mui/material";
+import TaskAltIcon from '@mui/icons-material/TaskAlt';
+import AddIcon from '@mui/icons-material/Add';
+import CloseIcon from '@mui/icons-material/Close';
+import { useState } from "react";
+
+const CloseButton = ({handleClose}: {handleClose: ()=> void}) => {
+    return <>
+    <AppBar sx={{ position: 'relative' }}>
+        <Toolbar>
+            <IconButton edge="start" color="inherit" onClick={handleClose}>
+            <CloseIcon/>
+            </IconButton>
+            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+              Lecture information
+            </Typography>
+        </Toolbar>
+    </AppBar>
+    </>
+}
+
+function EventInformation({name, date, destination, isOpened, setOpen, isAdded, updateSchedule}: {name : string, date : string, destination : string, 
+    isOpened: boolean, setOpen: (a: boolean) => void, isAdded: boolean, updateSchedule: (a : boolean) => void}) {
+    const handleClose = () => { setOpen(false); updateSchedule(isAdded)};
+      
+    return (
+    <div>
+        <Dialog onClose={handleClose} open={isOpened}>
+        <CloseButton handleClose={handleClose}/>
+        <DialogTitle>
+            {name}
+        </DialogTitle>
+        <DialogContent dividers>
+            <Typography>
+                Location: {destination}
+            </Typography>
+            <Typography>
+                {date}
+            </Typography>
+        </DialogContent>
+        <DialogActions>
+            <Button autoFocus onClick={handleClose}>
+            {isAdded ? "Remove from schedule":"Add to schedule"}
+            </Button>
+        </DialogActions>
+        </Dialog>
+    </div>
+    );
+}
+
+function SeminarListItem({name, date, destination}: {name : string, date : string, destination : string}) {
+    const [isAddedToSchedule, addSchedule] = useState(false);
+    const updateSchedule = (isAdded: boolean) => { addSchedule(!isAdded)}
+    const [infoIsDisplayed, displayEventInfo] = useState(false);
+    const setOpen = (isOpened: boolean) => { displayEventInfo(!infoIsDisplayed)}
+
+    return (
+        <ListItem alignItems="flex-start">
+            <Stack spacing={2} direction={"row"}>
+                <ListItemButton onClick={() => displayEventInfo(true)}>
+                    <ListItemText primary={date + " " + name} secondary={destination}/>
+                    <ListItemIcon sx={{display: 'flex', justifyContent:'flex-end'}}>
+                        {isAddedToSchedule ? <TaskAltIcon/> : <AddIcon/>}
+                    </ListItemIcon>
+                </ListItemButton>   
+            </Stack>
+            <EventInformation name={name} date= {date} isOpened={infoIsDisplayed} setOpen={setOpen} destination={destination} isAdded={isAddedToSchedule} updateSchedule={updateSchedule}/>
+        </ListItem>
+    );
+}
+
+function formatDate(date: Date) {
+    return date.getHours().toString() + ":" + date.getMinutes().toString().padStart(2, '0');
+}
+
+function SeminarTabs() {
+    const [value, setValue] = useState('1');
+  
+    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+      setValue(newValue);
+    };
+  
+    return (
+        <Grid container justifyContent={"center"} >
+            <Tabs value={value} onChange={handleChange}>
+                <Tab label="Day 1" value="1" />
+                <Tab label="Day 2" value="2" />
+            </Tabs>
+        </Grid>
+    );
+}
+
+export default function SeminarList() {
+    return (
+        <Grid container justifyContent="space-around" paddingTop={15} paddingBottom={15}>
+            <Paper elevation={3} >
+                <SeminarTabs/>
+                <List>
+                    <SeminarListItem name="seminarcic" destination="boulevard of broken dreams" date={formatDate(new Date())}/>
+                    <SeminarListItem name="seminarcic" destination="boulevard of broken dreams" date={formatDate(new Date())}/>
+                </List>
+            </Paper>
+        </Grid>
+    );
+}
