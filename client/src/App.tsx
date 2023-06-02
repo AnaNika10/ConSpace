0cb2be133b74da410fc4e794acd0bc703ebaef36
milicar7 <radojicicmilica98@gmail.com>
2023-06-03 00:17:39 +0200

    [#22] Add sign up and sign in component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,30 @@
 import { ThemeProvider } from "@emotion/react";
 import SeminarList from "./components/SeminarList";
-import SeminarCalendar from "./components/SeminarCalendar"
+import SeminarCalendar from "./components/SeminarCalendar";
 import { themeOptions } from "./components/DarkTheme";
 import { CssBaseline } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBarAndMenu from "./components/Menu";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
 
 function App() {
-  return  (
+  return (
     <>
       <ThemeProvider theme={themeOptions}>
-      <CssBaseline/>
-      <BrowserRouter>
-      <NavBarAndMenu/>
-      <Routes>
-        <Route path="/seminar-schedule" element={<SeminarList/>} />
-        <Route path="/calendar-schedule" element={<SeminarCalendar/>} />
-      </Routes>
-      </BrowserRouter>
+        <CssBaseline />
+        <BrowserRouter>
+          <NavBarAndMenu />
+          <Routes>
+            <Route path="/seminar-schedule" element={<SeminarList />} />
+            <Route path="/calendar-schedule" element={<SeminarCalendar />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/sign-in" element={<SignIn />} />
+          </Routes>
+        </BrowserRouter>
       </ThemeProvider>
     </>
-    
-  )
+  );
 }
 
 export default App;
--- client/src/components/NavigationBar.tsx
+++ client/src/components/NavigationBar.tsx
@@ -2,32 +2,31 @@ import { Button, Container, Grid, Stack, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 
 export default function NavBar() {
-
-    return (
-        <Container maxWidth={false}>
-            <Stack direction={"row"} justifyContent="space-around" alignItems="center">
-                <Typography  variant="h2" color={"#9c27b0"} fontFamily={"sans-serif"}>
-                    ConSpace
-                </Typography>
-                <Grid container justifyContent="flex-end">
-                    <Link to={"/seminar-schedule"}>
-                    <Button sx={{color: "white"}}>
-                        Seminar Schedule
-                    </Button>
-                    </Link>
-                    <Link to={"/exhibitors"}>
-                    <Button sx={{color: "white"}}>
-                        Exhibitors
-                    </Button>
-                    </Link>
-                    <Link to={"/floorplan"}>
-                    <Button sx={{color: "white"}}>
-                        Floorplan
-                    </Button>
-                    </Link>
-                    <Button variant="contained">Sign up</Button>
-                </Grid>
-            </Stack>
-        </Container>
-    )
+  return (
+    <Container maxWidth={false}>
+      <Stack
+        direction={"row"}
+        justifyContent="space-around"
+        alignItems="center"
+      >
+        <Typography variant="h2" color={"#9c27b0"} fontFamily={"sans-serif"}>
+          ConSpace
+        </Typography>
+        <Grid container justifyContent="flex-end">
+          <Link to={"/seminar-schedule"}>
+            <Button sx={{ color: "white" }}>Seminar Schedule</Button>
+          </Link>
+          <Link to={"/exhibitors"}>
+            <Button sx={{ color: "white" }}>Exhibitors</Button>
+          </Link>
+          <Link to={"/floorplan"}>
+            <Button sx={{ color: "white" }}>Floorplan</Button>
+          </Link>
+          <Link to={"/sign-up"}>
+            <Button variant="contained">Sign up</Button>
+          </Link>
+        </Grid>
+      </Stack>
+    </Container>
+  );
 }
