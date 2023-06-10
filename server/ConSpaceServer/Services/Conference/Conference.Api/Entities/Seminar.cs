9d7323f73ac5efc5a9e64786987c0ec86c7c9e5f
AnaNika10 <ana.nikacevic7@gmail.com>
2023-06-10 21:21:27 +0200

    {#40] Renamed conference controller to seminar

--- server/ConSpaceServer/Services/Conference/Conference.Api/Entities/Seminar.cs
+++ server/ConSpaceServer/Services/Conference/Conference.Api/Entities/Seminar.cs
@@ -5,7 +5,7 @@
             public int SeminarId { get; set; }
             public string Name { get; set; }
             public string Hall { get; set; }
-            public int[] Speakers { get; set; }
+            public string[] Speakers { get; set; }
             public int Exhibitors { get; set; }
             public string? Description { get; set; }
             public string[]? FilesUrls { get; set; }
