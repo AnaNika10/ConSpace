c40d85f95cab561d0e52c82c0843b44b83dc8ba4
marijal74 <57067366+marijal74@users.noreply.github.com>
2023-08-14 23:02:40 +0200

    Feature/#49 notes (#84)
    * [#49] Create notes component. Implement fetching, adding, updating and deleting notes
    
    * [#49] Fix fetching notes

--- client/src/dataProviders/UserDataProvider.ts
+++ client/src/dataProviders/UserDataProvider.ts
@@ -1,5 +1,6 @@
 import { AuthenticationWrapper } from './AuthenticationWrapper';
 import config from '../config/local.json'
+import { Note } from '../models/Note';
 
 export class UserDataProvider {
 
@@ -11,5 +12,21 @@ export class UserDataProvider {
 
     public static async deleteSeminarFromSchedule(seminarId : string, token : string) {
         return await AuthenticationWrapper.fetchWithAuth(`${this.gateway}/DeleteSeminarFromSchedule/${seminarId}`, "DELETE", token);
+    }
+
+    public static async getAllNotes(token : string) {
+        return await AuthenticationWrapper.fetchWithAuth(`${this.gateway}/GetAllNotes`, "GET", token);
+    }
+
+    public static async addNote(note:Note, token : string) {
+        return await AuthenticationWrapper.fetchWithAuth(`${this.gateway}/createNote`, "POST", token, note);
+    }
+
+    public static async deleteNote(id:string, token : string) {
+        return await AuthenticationWrapper.fetchWithAuth(`${this.gateway}/DeleteNote/${id}`, "DELETE", token);
+    }
+
+    public static async editNote(note: Note, token : string) {
+        return await AuthenticationWrapper.fetchWithAuth(`${this.gateway}/EditNote`, "PATCH", token, note);
     }
 }
