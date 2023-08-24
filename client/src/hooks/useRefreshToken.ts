4de6b216e81ea2705700452523b93f59c31018ab
milicar7 <radojicicmilica98@gmail.com>
2023-08-24 22:16:17 +0200

    [#83] Update hooks to functions

--- client/src/hooks/useRefreshToken.ts
+++ client/src/hooks/useRefreshToken.ts
@@ -3,33 +3,33 @@ import { REFRESH_URL } from '../constants/api';
 import useAuth from './useAuth';
 import jwt_decode from 'jwt-decode';
 
-const useRefreshToken = () => {
-    const { auth, setAuth } = useAuth();
+export default function useRefreshToken() {
+  const { auth, setAuth } = useAuth();
 
-    const decoded: any = auth?.accessToken
-    ? jwt_decode(auth.accessToken)
-    : undefined;
-    
-    const refresh = async () => {
-        const response = await axios.post(
-            REFRESH_URL,
-            JSON.stringify({
-              refreshToken: auth.refreshToken,
-              email: decoded?.Email,
-            }),
-            {
-              headers: { "Content-Type": "application/json" },
-            }
-        );
+  const decoded: any = auth?.accessToken
+  ? jwt_decode(auth.accessToken)
+  : undefined;
+  
+  async function refresh() {
+    const response = await axios.post(
+        REFRESH_URL,
+        JSON.stringify({
+          refreshToken: auth.refreshToken,
+          email: decoded?.Email,
+        }),
+        {
+          headers: { "Content-Type": "application/json" },
+        }
+    );
 
-        setAuth(prev => {
-            console.log(JSON.stringify(prev));
-            console.log(response.data.accessToken);
-            return { accessToken: response.data.accessToken, refreshToken: response.data.refreshToken }
-        });
-        return response.data.accessToken;
-    }
-    return refresh;
-};
-
-export default useRefreshToken;
+    console.log("refresh")
+    setAuth(prev => {
+        console.log(JSON.stringify(prev));
+        console.log(response.data.accessToken);
+        return { accessToken: response.data.accessToken, refreshToken: response.data.refreshToken }
+    });
+    return response.data.accessToken;
+  }
+  
+  return refresh;
+}
