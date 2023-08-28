964211114cc9d34bbcda153a43c41e6ff6dadb45
marijal74 <lakic.marija98@gmail.com>
2023-08-28 18:56:31 +0200

    [#45] Add email column in attendee table and consume email field from identity service

--- server/ConSpaceServer/Services/User/User.Common/Migrations/20230828165535_AddEmailColumnInAttendeeTable.Designer.cs
+++ server/ConSpaceServer/Services/User/User.Common/Migrations/20230828165535_AddEmailColumnInAttendeeTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using User.Common.Data;
@@ -12,9 +13,11 @@ using User.Common.Data;
 namespace User.Migrations
 {
     [DbContext(typeof(UserContext))]
-    partial class UserContextModelSnapshot : ModelSnapshot
+    [Migration("20230828165535_AddEmailColumnInAttendeeTable")]
+    partial class AddEmailColumnInAttendeeTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
