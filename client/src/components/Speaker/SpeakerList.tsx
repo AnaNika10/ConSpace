7024f5adc7eed7d18ecfb83044e5a576720c57d4
marijal74 <lakic.marija98@gmail.com>
2023-08-29 00:02:25 +0200

    [#46] Fix rendering of speakers list

--- client/src/components/Speaker/SpeakerList.tsx
+++ client/src/components/Speaker/SpeakerList.tsx
@@ -1,9 +1,18 @@
-import { AppBar, Fab, Grid, IconButton, List, Paper, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Fab,
+  Grid,
+  IconButton,
+  List,
+  Paper,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { useEffect, useState } from "react";
 import { SpeakerDataProvider } from "../../dataProviders/SpeakerDataProvider";
 import { Speaker } from "../../models/Speaker";
 import { SpeakerItem } from "./SpeakerItem";
-import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
 import useAuth from "../../hooks/useAuth";
 import { SpeakerForm } from "./SpeakerForm";
 import { Add } from "@mui/icons-material";
@@ -12,13 +21,13 @@ import withSnackbar from "../Common/SnackBarWrapper";
 import { useNavigate, useLocation } from "react-router-dom";
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 
-const emptySpeaker : Speaker = {
+const emptySpeaker: Speaker = {
   name: "",
   position: "",
   company: "",
-  bioInfo : "",
-  email : ""
-}
+  bioInfo: "",
+  email: "",
+};
 const fabStyle = {
   position: "absolute",
   bottom: 16,
@@ -29,15 +38,18 @@ const AddButton = () => {
 
   return (
     <>
-        <Toolbar>
-
-          <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
-            List of speakers
-          </Typography>
-          <IconButton edge="end" color="inherit" onClick={() => setOpenForm(true)}>
-            <AddCircleOutlineOutlinedIcon />
-          </IconButton>
-          {/* { <SpeakerForm
+      <Toolbar>
+        <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+          List of speakers
+        </Typography>
+        <IconButton
+          edge="end"
+          color="inherit"
+          onClick={() => setOpenForm(true)}
+        >
+          <AddCircleOutlineOutlinedIcon />
+        </IconButton>
+        {/* { <SpeakerForm
          
          isOpened={openForm}
          displayEventInfo={() => setOpenForm(false)}
@@ -45,21 +57,23 @@ const AddButton = () => {
          setMessage={setMessage}
 
        /> } */}
-        </Toolbar>
+      </Toolbar>
     </>
   );
 };
 
-function SeminarList({
-  setMessage
+function SpeakersList({
+  setMessage,
 }: {
-  speaker : Speaker
+  speaker: Speaker;
   isOpened: boolean;
-  displayEventInfo : (a:boolean) => void;
-  setMessage: (msg: string) => void
+  displayEventInfo: (a: boolean) => void;
+  setMessage: (msg: string) => void;
 }) {
   const { auth } = useAuth();
-  const decoded :any = auth?.accessToken ? jwtDecode(auth.accessToken) : undefined;
+  const decoded: any = auth?.accessToken
+    ? jwtDecode(auth.accessToken)
+    : undefined;
   const role = decoded?.Role || "";
   const isAdmin = role === "Administrator";
   const [data, setData] = useState<Speaker[]>([]);
@@ -104,7 +118,6 @@ function SeminarList({
     };
   }, [location.pathname, data, auth]);
 
-
   return (
     <Grid
       container
@@ -113,31 +126,35 @@ function SeminarList({
       paddingBottom={15}
     >
       <Paper elevation={3}>
-      { isAdmin  && <AddButton /> }
+        {isAdmin && <AddButton />}
         <List>
           {!isLoading &&
             data.map((speaker: Speaker) => {
               return (
-                <SpeakerItem  speaker={speaker} setMessage={setMessage}  key={speaker.speakerId} />
+                <SpeakerItem
+                  speaker={speaker}
+                  setMessage={setMessage}
+                  key={speaker.speakerId}
+                />
               );
             })}
         </List>
-      { isAdmin &&  <Fab sx={fabStyle} color="primary" aria-label="add">
-        <Add onClick={handleOpen} />
-      </Fab> }
-      { isAdmin && !isLoading && !emptySpeaker && <SpeakerForm
-         
-         isOpened={openForm}
-         displayEventInfo={() => setOpenForm(false)}
-         speaker={emptySpeaker}
-         setMessage={setMessage}
-
-       /> }
+        {isAdmin && (
+          <Fab sx={fabStyle} color="primary" aria-label="add">
+            <Add onClick={handleOpen} />
+          </Fab>
+        )}
+        {isAdmin && !isLoading && !emptySpeaker && (
+          <SpeakerForm
+            isOpened={openForm}
+            displayEventInfo={() => setOpenForm(false)}
+            speaker={emptySpeaker}
+            setMessage={setMessage}
+          />
+        )}
       </Paper>
-
     </Grid>
-    
   );
 }
 
-export default withSnackbar(SpeakerForm);
+export default withSnackbar(SpeakersList);
